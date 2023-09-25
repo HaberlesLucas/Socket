3eb061f76e2955045079c136c340c8f877e94551
Lucas Haberles <lucashaberles41q@gmail.com>
2023-09-25 18:19:36 -0300

    nuevas actualizaciones y diagrama de secuencia

--- Server/Server.py
+++ Server/Server.py
@@ -1,63 +1,3 @@
-# import socket
-# import threading # para los hilos 
-
-# # ip y puerto del servidor 
-# host = '127.0.0.1' 
-# port = 1812     
-
-# #para almacenar los clientes conectados
-# clientes = []
-
-# #f para manejar la comunicación con un cliente
-# def manejar_cliente(cliente, direccion):
-#     while True:
-#         try:
-#             mensaje = cliente.recv(1024).decode('utf-8')
-#             if mensaje:
-#                 if mensaje == "/listar":
-#                     #enviar la lista de usuarios conectados al cliente que la solicita
-#                     lista_usuarios = ", ".join([str(c.getpeername()) for c in clientes])
-#                     cliente.send(f'Usuarios conectados: {lista_usuarios}'.encode('utf-8'))
-#                 elif mensaje == "/quitar":
-#                     #desconectar al cliente que envía el comando "/quitar"
-#                     cliente.close()
-#                     clientes.remove(cliente)
-#                     break
-#                 else:
-#                     #reenviar el mensaje a todos los clientes
-                    
-#                     #'{mensaje} contiene en name_user y mensaje => 'Nuevo mensaje de {mensaje}' mostrará una 
-#                     # salida como >> "Nuevo mensaje de Lucas: contenido del mensaje"
-#                     mensaje_a_transmitir = f'Nuevo mensaje de {mensaje}' 
-#                     for c in clientes:
-#                         if c != cliente:
-#                             c.send(mensaje_a_transmitir.encode('utf-8'))
-#             else:
-#                 #si no hay datos, desconectar al cliente
-#                 cliente.close()
-#                 clientes.remove(cliente)
-#                 break
-#         except:
-#             #si ocurre un error, desconectar al cliente
-#             cliente.close()
-#             clientes.remove(cliente)
-#             break
-
-
-# #socket del servidor
-# servidor = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# servidor.bind((host, port))
-# servidor.listen(5)
-# print(f'Servidor escuchando en {host}:{port}')
-
-# #aceptar conexiones entrantes y crear hilos para manejar a los clientes
-# while True:
-#     cliente, direccion = servidor.accept()
-#     print(f'Nueva conexión de {direccion}')
-#     clientes.append(cliente)
-#     cliente_thread = threading.Thread(target=manejar_cliente, args=(cliente, direccion))
-#     cliente_thread.start()
-
 import socket
 import threading
 
@@ -69,7 +9,7 @@ port = 1812
 clientes = []
 
 # función para enviar un mensaje a todos los clientes
-def broadcast(mensaje, cliente_enviador=None):
+def transmision(mensaje, cliente_enviador=None):
     for cliente in clientes:
         if cliente != cliente_enviador:
             try:
@@ -93,23 +33,23 @@ def manejar_cliente(cliente, direccion):
                     # desconectar al cliente que envía el comando "/quitar"
                     cliente.close()
                     clientes.remove(cliente)
-                    broadcast(f'Cliente {direccion} se ha desconectado.\n'.encode('utf-8'))
+                    transmision(f'Cliente {direccion} se ha desconectado.\n'.encode('utf-8'))
                     break
                 else:
                     # reenviar el mensaje a todos los clientes
                     mensaje_a_transmitir = f'Nuevo mensaje de {direccion}: {mensaje}\n'.encode('utf-8')
-                    broadcast(mensaje_a_transmitir, cliente)
+                    transmision(mensaje_a_transmitir, cliente)
             else:
                 # si no hay datos, desconectar al cliente
                 cliente.close()
                 clientes.remove(cliente)
-                broadcast(f'Cliente {direccion} se ha desconectado.\n'.encode('utf-8'))
+                transmision(f'Cliente {direccion} se ha desconectado.\n'.encode('utf-8'))
                 break
         except:
             # si ocurre un error, desconectar al cliente
             cliente.close()
             clientes.remove(cliente)
-            broadcast(f'Cliente {direccion} se ha desconectado.\n'.encode('utf-8'))
+            transmision(f'Cliente {direccion} se ha desconectado.\n'.encode('utf-8'))
             break
 
 # socket del servidor
@@ -123,6 +63,6 @@ while True:
     cliente, direccion = servidor.accept()
     print(f'Nueva conexión de {direccion}')
     clientes.append(cliente)
-    broadcast(f'Cliente {direccion} se ha conectado.\n'.encode('utf-8'))
+    transmision(f'Cliente {direccion} se ha conectado.\n'.encode('utf-8'))
     cliente_thread = threading.Thread(target=manejar_cliente, args=(cliente, direccion))
     cliente_thread.start()
